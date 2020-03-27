747109d1527de3726e8cdc175a3b5e534dc5c0a6
sujonhossainbd <sujon.hossain758@gmail.com>
2020-03-27 07:41:18 +0600

    Javascript build in Math method

---
+++ Basic JavaScript/Math.js
@@ -0,0 +1,18 @@
+console.log(Math.E);
+console.log(Math.PI);
+var n = 45.2523;
+
+console.log(Math.abs(n));
+console.log(Math.floor(n));
+console.log(Math.ceil(n));
+console.log(Math.round(n));
+
+console.log(Math.max(45,23, 253, 25));
+console.log(Math.min(23,253,35, 353,25));
+
+let arr = [23, 253, 253, 253,2532, 253,32, 353,633,33]
+console.log(Math.max(...arr));
+
+console.log(Math.sqrt(Math.max(...arr)));
+console.log(Number.parseInt(Math.random()* 100 + 1));
+                                                                                  
