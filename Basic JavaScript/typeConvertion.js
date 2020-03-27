023b124d52d40f50596f16471868020d7b659594
sujonhossainbd <sujon.hossain758@gmail.com>
2020-03-27 06:57:02 +0600

    type converstion and true and falsy value

---
+++ Basic JavaScript/typeConvertion.js
@@ -0,0 +1,28 @@
+
+var str = '1000'
+var number = 19
+
+var total = Number.parseInt(str) + number;
+console.log(total);
+
+console.log(number.toString());
+
+
+// falsy value
+// ''
+// 0
+// null
+// undefined
+// NaN
+
+console.log(Boolean(''));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean(NaN));
+console.log(Boolean(0));
+
+var x = true;
+
+console.log(x.toString());
+
+Constructor, Number, String, Boolean
