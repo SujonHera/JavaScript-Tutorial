356c675bdd84d1d76ba8b697ede9414f88e5f568
sujonhossainbd <sujon.hossain758@gmail.com>
2020-03-27 08:18:37 +0600

    if else

---
+++ Basic JavaScript/if-else.js
@@ -0,0 +1,16 @@
+let a = 10
+let b = 20
+
+if(a > b){
+    console.log("a is biggest")
+}else{
+    console.log("b is biggest")
+}
+
+let num = 20;
+
+if(num % 2 == 0){
+    console.log(`${num} is even number`); 
+}else{
+    console.log(`${num} is odd`)
+}
