8396dbc36ce4d469d19cc48ae89b848648ba102f
sujonhossainbd <sujon.hossain758@gmail.com>
2020-03-27 07:55:11 +0600

    date functional

---
+++ Basic JavaScript/Date.js
@@ -0,0 +1,18 @@
+let date = new Date()
+console.log(date);
+
+console.log(date.toDateString()); // full date 
+console.log(date.toTimeString()); 
+console.log(date.toLocaleDateString());
+console.log(date.toLocaleString());
+
+
+console.log(date.getDate());
+console.log(date.getDay());
+console.log(date.getHours());
+console.log(date.getMinutes());
+console.log(date.getSeconds());
+console.log(date.getFullYear());
+console.log(date.getTime());
+
+
